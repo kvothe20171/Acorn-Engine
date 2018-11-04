31f8c45d51901f6f4ef7913b5e2f71dde9f428eb
Ben Corekin <corekinben@gmail.com>
2018-11-04 12:52:39 -0800

    Added Logging system!

--- src/Acorn.h
+++ src/Acorn.h
@@ -4,6 +4,7 @@
 
 #include "Acorn/Application.h"
 #include "Acorn/Core.h"
+#include "Acorn/Log.h"
 
 // ---Entry Point ----------
 #include "Acorn/EntryPoint.h"
--- src/Acorn/EntryPoint.h
+++ src/Acorn/EntryPoint.h
@@ -6,6 +6,11 @@ extern Acorn::Application* Acorn::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Acorn::Log::Init();
+	AC_CORE_WARN("Initialized Log!");
+	int a = 5;
+	AC_INFO("Hello! Var={0}", a);
+
 	auto app = Acorn::CreateApplication();
 	app->Run();
 	delete app;
